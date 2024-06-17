a8d8fa0222433f91e3d7e2c28dab1e0929f9128e
satyagilegitbytes <satyagi@legitbytes.com>
2024-06-17 12:38:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,56 +1,41 @@
 # main.tf
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "ap-southeast-1"  # Use the region specified in your Jenkins environment
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "${var.bucket_name_prefix}-terraform-test-bucket"
-  force_destroy = true # Enables force deletion of the bucket for testing
+variable "user_id" {}
+variable "game_id" {}
 
-  tags = {
-    Name        = "My S3 Bucket"
-    Environment = "Test"
-  }
+resource "aws_s3_bucket" "game_bucket" {
+  bucket = "game-build-bucket-${var.user_id}-${var.game_id}"
+  acl    = "public-read"
 }
 
-resource "aws_s3_bucket_acl" "my_bucket_acl" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-  acl    = "private"
+resource "aws_s3_bucket_object" "game_build" {
+  bucket = aws_s3_bucket.game_bucket.bucket
+  key    = "game-build.zip"
+  source = "game-build.zip"  # This should be the path to the actual build file
 }
 
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
-      }
-    ]
-  })
-}
-
-resource "aws_cloudfront_distribution" "my_distribution" {
+resource "aws_cloudfront_distribution" "cdn" {
   origin {
-    domain_name = aws_s3_bucket.my_bucket.bucket_regional_domain_name
-    origin_id   = aws_s3_bucket.my_bucket.id
+    domain_name = aws_s3_bucket.game_bucket.bucket_regional_domain_name
+    origin_id   = "${var.user_id}-${var.game_id}"
+
+    s3_origin_config {
+      origin_access_identity = "origin-access-identity/cloudfront/EXAMPLE"
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = ""
+  default_root_object = "index.html"
 
   default_cache_behavior {
-    target_origin_id       = aws_s3_bucket.my_bucket.id
-    viewer_protocol_policy = "redirect-to-https"
-
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods  = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.user_id}-${var.game_id}"
 
     forwarded_values {
       query_string = false
@@ -59,9 +44,10 @@ resource "aws_cloudfront_distribution" "my_distribution" {
       }
     }
 
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 86400
+    max_ttl                = 31536000
   }
 
   restrictions {
@@ -73,16 +59,8 @@ resource "aws_cloudfront_distribution" "my_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
-
-  tags = {
-    Name = "My CloudFront Distribution"
-  }
-}
-
-variable "bucket_name_prefix" {
-  default = "02"  # Replace with a unique prefix
 }
 
 output "cdn_url" {
-  value = aws_cloudfront_distribution.my_distribution.domain_name
+  value = aws_cloudfront_distribution.cdn.domain_name
 }
