2b707c17d9c3a6f80bea4172aeaec179e306be6a
satyagilegitbytes <satyagi@legitbytes.com>
2024-06-17 11:41:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,54 @@
+# main.tf
+
 provider "aws" {
-  region = "your_aws_region"
+  region = "ap-southeast-1"  # Ensure this is set to a valid AWS region
 }
 
-resource "aws_s3_bucket" "terraform_test_bucket" {
-  bucket = "terraform-test-bucket"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "terraform-test-bucket"  # Replace with a unique name
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_object" "test_folder" {
-  bucket = aws_s3_bucket.terraform_test_bucket.bucket
-  key    = "test/"
-  acl    = "private"
+resource "aws_cloudfront_distribution" "my_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.my_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.my_bucket.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = ""
+
+  default_cache_behavior {
+    target_origin_id       = aws_s3_bucket.my_bucket.id
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "cdn_url" {
+  value = aws_cloudfront_distribution.my_distribution.domain_name
 }
