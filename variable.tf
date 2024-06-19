b176e3e9be835226a94472bce756dc14f2f6b6d3
satyagilegitbytes <satyagi@legitbytes.com>
2024-06-19 12:24:27 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "user_id" {
+  description = "User ID for the application"
+  type        = string
+  default     = "testuser"
+}
+
+variable "game_id" {
+  description = "Game ID for the application"
+  type        = string
+  default     = "testgameid"
+}
